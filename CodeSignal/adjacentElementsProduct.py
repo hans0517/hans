42b1dacaaa93922b9c7a1a933715f0a1c409bd45
hans0517 <55570330+hans0517@users.noreply.github.com>
2019-10-06 14:23:14 +0800

    Update adjacentElementsProduct.py

--- CodeSignal/adjacentElementsProduct.py
+++ CodeSignal/adjacentElementsProduct.py
@@ -1,4 +1,4 @@
 def adjacentElementsProduct(inputArray):
-    abc = [inputArray[i]*inputArray[i+1]
-           for i in range(0, len(inputArray)-1)]
-    return max(abc)
+    abc = [inputArray[i]*inputArray[i+1] #做一個List為前一項和後一項相乘
+           for i in range(0, len(inputArray)-1)] #利用迴圈跑完inputArray裡的前一項和後一項相乘
+    return max(abc) #回傳題目所需，abc裡的最大值
