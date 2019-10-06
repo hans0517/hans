4ac0bf4928b800fafc2b653a8c9cd9c8744d66bd
hans0517 <55570330+hans0517@users.noreply.github.com>
2019-10-06 14:29:10 +0800

    Update checkPalindrome.py

--- CodeSignal/checkPalindrome.py
+++ CodeSignal/checkPalindrome.py
@@ -1,7 +1,7 @@
 def checkPalindrome(inputString):
     def reverse(inputString):
-        return inputString[::-1]
-    if inputString == reverse(inputString):
+        return inputString[::-1] #先把inputString轉向
+    if inputString == reverse(inputString): #如果轉向完後相等，回傳True，此外則為False
         return True
     else:
         return False
