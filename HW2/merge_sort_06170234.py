540c651a0db7a05f19a82d2b25be4410cde33c99
hans0517 <55570330+hans0517@users.noreply.github.com>
2019-11-08 08:29:33 +0800

    Update merge_sort_06170234.py

--- HW2/merge_sort_06170234.py
+++ HW2/merge_sort_06170234.py
@@ -14,19 +14,20 @@ class Solution(object):
                 if left[i] < right[j]:
                     nums[k] = left[i]
                     i = i+1 #如果左邊的數小於右邊的，將左邊的數加到空的nums的k
+                    k = k+1
                 else:
                     nums[k] = right[j]
                     j = j+1 #跟以上同邏輯，右邊的加進去
-                k = k+1 #k要跟著移動，加完一個要繼續到下一個位置
-            while i < len(left):
-                nums[k] = left[i]
-                i = i+1
-                k = k+1
-
-            while j < len(right):
-                nums[k] = right[j]
-                j = j+1
-                k = k+1
+                    k = k+1 #k要跟著移動，加完一個要繼續到下一個位置
+            while i < len(left) or j < len(right):
+                if i < len(left):
+                    nums[k] = left[i]
+                    i = i+1
+                    k = k+1
+                if j < len(right):
+                    nums[k] = right[j]
+                    j = j+1
+                    k = k+1
             return nums
           
 nums = [-2, 6, 7, -4, 0, 3, -1, 4]
