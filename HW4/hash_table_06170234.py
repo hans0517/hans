4578a36be7150e314429efa2f963497ca8f310f9
hans0517 <55570330+hans0517@users.noreply.github.com>
2019-12-07 23:53:48 +0800

    Update hash_table_06170234.py

--- HW4/hash_table_06170234.py
+++ HW4/hash_table_06170234.py
@@ -30,18 +30,26 @@ class MyHashSet:
 
 
     def remove(self, key):
-        h = MD5.new()
-        h.update(key.encode('utf-8'))
-        key = int(h.hexdigest(), 16)
-        index = key % self.capacity
-        cur_node = self.data[index]
-        if cur_node==None:
-            return
-        if self.data[index].val == key:
-            self.data[index] = self.data[index].next
-            return
-        while cur_node.next.val != key:
-            cur_node = cur_node.next
+        if self.contains(key) == True:
+            h = MD5.new()
+            h.update(key.encode('utf-8'))
+            key = int(h.hexdigest(), 16)
+            index = key % self.capacity
+            cur_node = self.data[index]
+            if cur_node==None:
+                pass
+            if self.data[index].val == key:
+                self.data[index] = self.data[index].next
+                return
+            elif cur_node.next != None:
+                while cur_node.next:
+                    if cur_node.next.val == key:
+                        cur_node.next = cur_node.next.next
+                    else:
+                        cur_node = cur_node.next
+                return
+        else:
+            pass
         
         
     def contains(self, key):
