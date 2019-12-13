70b5ad4f8fbcd1b3ffcf6f9ee6a5e44d57974f8a
hans0517 <55570330+hans0517@users.noreply.github.com>
2019-12-13 11:10:03 +0800

    Update MinStack.py

--- week3/MinStack.py
+++ week3/MinStack.py
@@ -1,1 +1,22 @@
+class MinStack:
 
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.len = 0
+        self.list = []
+
+    def push(self, x: int) -> None:
+        self.list.append(x)
+        self.len+=1
+
+    def pop(self) -> None:
+        self.list.pop(self.len-1)
+        self.len-=1
+
+    def top(self) -> int:
+        return self.list[-1]
+
+    def getMin(self) -> int:
+        return min(self.list)
