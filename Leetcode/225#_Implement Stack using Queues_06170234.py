c82a216a6f911974a37d30c52ef269a0e96aeb8d
hans0517 <55570330+hans0517@users.noreply.github.com>
2020-01-10 10:36:30 +0800

    Create 225#_Implement Stack using Queues_06170234.py

---
+++ Leetcode/225#_Implement Stack using Queues_06170234.py
@@ -0,0 +1,19 @@
+class MyStack:
+
+    def __init__(self):
+        self.list = []
+
+    def push(self, x: int) -> None:
+        self.list.append(x)
+
+    def pop(self) -> int:
+        return self.list.pop()
+
+    def top(self) -> int:
+        return self.list[-1]
+
+    def empty(self) -> bool:
+        if len(self.list) == 0:
+            return True
+        else:
+            return False
